6d7b84f3b3f73b2a126dfc733db575183be47e80
Muki Kiboigo <muki@kiboigo.com>
2024-06-30 23:10:17 -0700

    fully parsing request

--- src/main.zig
+++ src/main.zig
@@ -1,36 +1,35 @@
 const std = @import("std");
-const Header = @import("header.zig").Header;
+const Request = @import("request.zig").Request;
 const stdout = std.io.getStdOut().writer();
 
-var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
-var allocator = arena.allocator();
-
 pub fn main() !void {
-    defer arena.deinit();
-
     const port: u16 = 9862;
 
     const addr = try std.net.Address.resolveIp("127.0.0.1", port);
     try stdout.print("Starting Z3 Server...\n", .{});
     var server = try addr.listen(.{ .reuse_port = true });
     defer server.deinit();
+
     try stdout.print("Started Z3 Server. (Port: {d})\n", .{port});
 
     while (std.net.Server.accept(&server)) |conn| {
         try stdout.print("Connection Accepted (Addr: {any})\n", .{conn.address});
         const thread = try std.Thread.spawn(.{}, struct {
             fn thread_request(connection: std.net.Server.Connection) !void {
+                var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+                const allocator = arena.allocator();
+                defer arena.deinit();
                 defer connection.stream.close();
+
+                // Basically, the stream will be passed around from request and response.
                 var stream = connection.stream;
 
-                var buffer: [1024]u8 = undefined;
-                var buf = std.io.bufferedReader(stream.reader());
-                const msg = try buf.reader().readUntilDelimiter(&buffer, '\n');
+                var request = Request.init(allocator);
+                try request.parse(stream);
 
-                var head = Header.init(allocator);
-                defer head.deinit();
-                try head.parse(msg);
-                try stream.writeAll("HTTP/1.0 200 OK");
+                const file = @embedFile("./sample.html");
+                try stream.writeAll("HTTP/1.0 200 OK\r\n\r\n");
+                try stream.writeAll(file);
             }
         }.thread_request, .{conn});
         thread.detach();
