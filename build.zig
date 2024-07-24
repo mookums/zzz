7f225adf0dbc194a65472185240223b003fa6f53
Muki Kiboigo <muki@kiboigo.com>
2024-07-24 15:58:08 -0700

    clean up old artficacts

--- build.zig
+++ build.zig
@@ -10,19 +10,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
-    // For the C allocator for now...
-    exe.linkLibC();
     b.installArtifact(exe);
-
-    const base = b.addExecutable(.{
-        .name = "base",
-        .root_source_file = b.path("src/base.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    b.installArtifact(base);
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
@@ -30,17 +18,8 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const base_cmd = b.addRunArtifact(base);
-    base_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        base_cmd.addArgs(args);
-    }
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    const base_step = b.step("base", "Run stdlib HTTP Server");
-    base_step.dependOn(&base_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
