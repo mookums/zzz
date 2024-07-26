77231fd77a363f64e66395b472c8439acbdcc235
Muki Kiboigo <muki@kiboigo.com>
2024-07-26 01:34:46 -0700

    FINALLY, doing a module style

---
+++ src/examples/http/main.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const log = std.log.scoped(.@"examples/http");
+const zzz = @import("zzz").Servers.TCP.HTTP;
+
+pub fn main() !void {
+    const host: []const u8 = "0.0.0.0";
+    const port: u16 = 9862;
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var router = zzz.Router.init(allocator);
+    try router.serve_embedded_file("/", zzz.Mime.HTML, @embedFile("sample.html"));
+
+    var server = zzz.Server.init(.{ .allocator = allocator }, router);
+    try server.bind(host, port);
+    try server.listen();
+}
