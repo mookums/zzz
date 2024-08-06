d11ff0acee35cd52098f7f5e041f7aa037531c80
Muki Kiboigo <muki@kiboigo.com>
2024-08-05 21:22:27 -0700

    use thread-safe allocator

--- src/examples/multithread/main.zig
+++ src/examples/multithread/main.zig
@@ -6,32 +6,17 @@ pub fn main() !void {
     const host: []const u8 = "0.0.0.0";
     const port: u16 = 9862;
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
-    defer arena.deinit();
+    const allocator = std.heap.page_allocator;
 
     var router = zzz.Router.init(allocator);
-    try router.serve_route("/", zzz.Route.init().get(struct {
-        pub fn handler_fn(request: zzz.Request, context: zzz.Context) zzz.Response {
-            _ = context;
-            const body =
-                \\ <!DOCTYPE html>
-                \\ <html>
-                \\ <body>
-                \\ <h1>Hello, World!</h1>
-                \\ </body>
-                \\ </html>
-            ;
-
-            _ = request;
-            return zzz.Response.init(.OK, zzz.Mime.HTML, body[0..]);
-        }
-    }.handler_fn));
+    try router.serve_embedded_file("/", zzz.Mime.HTML, @embedFile("index.html"));
 
     var server = zzz.Server.init(.{
+        // if multithreaded, you need a thread-safe allocator.
         .allocator = allocator,
         .threading = .{ .multi_threaded = 4 },
     }, router);
+    defer server.deinit();
     try server.bind(host, port);
     try server.listen();
 }
