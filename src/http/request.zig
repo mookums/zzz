27ebcdd735fea4191256e8d785c762631a48ecf3
Muki Kiboigo <muki@kiboigo.com>
2024-08-08 15:24:36 -0700

    use new parsing method by default

--- src/http/request.zig
+++ src/http/request.zig
@@ -28,118 +28,7 @@ pub const Request = struct {
         };
     }
 
-    // This is for parsing it out of a buffer.
-    pub fn parse(options: RequestOptions, buffer: []const u8) !Request {
-        var request = Request.init(options);
-        // Parse the Request here.
-        const RequestLineParsing = enum {
-            Method,
-            Host,
-            Version,
-        };
-
-        const HeaderParsing = enum {
-            Name,
-            Value,
-        };
-
-        const ParsingStages = enum {
-            RequestLine,
-            Headers,
-        };
-
-        const Parsing = union(ParsingStages) {
-            RequestLine: RequestLineParsing,
-            Headers: HeaderParsing,
-        };
-
-        var stage: Parsing = .{ .RequestLine = .Method };
-
-        var start: usize = 0;
-        var no_bytes_left = false;
-        var key_value: KVPair = .{ .key = undefined, .value = undefined };
-
-        parse: for (0..request.options.request_max_size) |i| {
-            // Our byte is either valid or we set the no_bytes_left flag.
-            const byte = blk: {
-                if (i < buffer.len) {
-                    break :blk buffer[i];
-                } else {
-                    no_bytes_left = true;
-                    break :blk 0;
-                }
-            };
-
-            switch (stage) {
-                .RequestLine => |rl| {
-                    if (std.ascii.isWhitespace(byte) or no_bytes_left) {
-                        switch (rl) {
-                            .Method => {
-                                request.add_method(@enumFromInt(std.http.Method.parse(buffer[start..i])));
-                                start = i;
-                                stage = .{ .RequestLine = .Host };
-                            },
-
-                            .Host => {
-                                request.add_host(buffer[start + 1 .. i]);
-                                start = i;
-                                stage = .{ .RequestLine = .Version };
-                            },
-
-                            .Version => {
-                                // TODO: Parse This.
-                                request.add_version(std.http.Version.@"HTTP/1.1");
-                                start = i + 1;
-                                stage = .{ .Headers = .Name };
-                            },
-                        }
-                    }
-                },
-
-                .Headers => |h| {
-                    // Possible Delimters...
-                    if (byte == ':' or byte == '\n' or no_bytes_left) {
-                        switch (h) {
-                            .Name => {
-                                if (byte == '\r' or byte == '\n') {
-                                    continue;
-                                }
-
-                                if (byte != ':') {
-                                    break :parse;
-                                }
-
-                                const key = std.mem.trimLeft(u8, buffer[start..i], &std.ascii.whitespace);
-                                key_value.key = key;
-
-                                // We want to skip the colon.
-                                start = i + 1;
-                                stage = .{ .Headers = .Value };
-                            },
-
-                            .Value => {
-                                // Ignore colons in the Header Value.
-                                if (byte == ':') {
-                                    continue;
-                                }
-
-                                const value = std.mem.trimLeft(u8, buffer[start..i], &std.ascii.whitespace);
-                                key_value.value = value;
-
-                                try request.add_header(key_value);
-                                start = i;
-                                stage = .{ .Headers = .Name };
-                            },
-                        }
-                    }
-                },
-            }
-        }
-
-        return request;
-    }
-
-    pub fn parse2(options: RequestOptions, bytes: []const u8) !Request {
+    pub fn parse(options: RequestOptions, bytes: []const u8) !Request {
         var request = Request.init(options);
 
         var total_size: u32 = 0;
@@ -210,7 +99,7 @@ test "Parse Request" {
         \\Accept: text/html
     ;
 
-    const request = try Request.parse2(.{}, request_text[0..]);
+    const request = try Request.parse(.{}, request_text[0..]);
 
     try testing.expectEqual(.GET, request.method);
     try testing.expectEqualStrings("/", request.host);
