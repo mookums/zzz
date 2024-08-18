18adfe92ff4262e9adcbbd0d26c12f8ce71f30fb
Muki Kiboigo <muki@kiboigo.com>
2024-08-17 18:47:12 -0700

    use async backend for io_uring by default

--- src/async/io_uring.zig
+++ src/async/io_uring.zig
@@ -3,45 +3,43 @@ const Completion = @import("completion.zig").Completion;
 const Async = @import("lib.zig").Async;
 const AsyncError = @import("lib.zig").AsyncError;
 
-pub const AsyncIoUring = struct {
-    pub fn init(size_connections_max: u32, params: std.os.linux.io_uring_params) AsyncIoUring {
-        // This should initialize IO_Uring.
-        var uring = try std.os.linux.IoUring.init_params(
-            size_connections_max,
-            &params,
-        );
+const log = std.log.scoped(.@"async/io_uring");
 
+pub const AsyncIoUring = struct {
+    runner: *anyopaque,
+    completions: [256]Completion,
+
+    pub fn init(uring: *std.os.linux.IoUring) !AsyncIoUring {
         return AsyncIoUring{
-            .runner = &uring,
-            .cqes = [_]std.os.linux.io_uring_cqe{undefined} ** 256,
+            .runner = uring,
             .completions = [_]Completion{undefined} ** 256,
         };
     }
 
     pub fn queue_accept(self: *Async, context: *anyopaque, socket: std.posix.socket_t) AsyncError!void {
-        const uring: *std.os.linux.IoUring = @ptrCast(self.runner);
-        _ = try uring.accept(@as(u64, @intFromPtr(context)), socket, null, null, 0);
+        const uring: *std.os.linux.IoUring = @ptrCast(@alignCast(self.runner));
+        _ = uring.accept(@as(u64, @intFromPtr(context)), socket, null, null, 0) catch unreachable;
     }
 
     pub fn queue_recv(self: *Async, context: *anyopaque, socket: std.posix.socket_t, buffer: []u8) AsyncError!void {
-        const uring: *std.os.linux.IoUring = @ptrCast(self.runner);
-        _ = try uring.recv(@as(u64, @intFromPtr(context)), socket, .{ .buffer = buffer }, 0);
+        const uring: *std.os.linux.IoUring = @ptrCast(@alignCast(self.runner));
+        _ = uring.recv(@as(u64, @intFromPtr(context)), socket, .{ .buffer = buffer }, 0) catch unreachable;
     }
 
-    pub fn queue_send(self: *Async, context: *anyopaque, socket: std.posix.socket_t, buffer: []u8) AsyncError!void {
-        const uring: *std.os.linux.IoUring = @ptrCast(self.runner);
-        _ = try uring.send(@as(u64, @intFromPtr(context)), socket, buffer, 0);
+    pub fn queue_send(self: *Async, context: *anyopaque, socket: std.posix.socket_t, buffer: []const u8) AsyncError!void {
+        const uring: *std.os.linux.IoUring = @ptrCast(@alignCast(self.runner));
+        _ = uring.send(@as(u64, @intFromPtr(context)), socket, buffer, 0) catch unreachable;
     }
 
     pub fn submit(self: *Async) AsyncError!void {
-        const uring: *std.os.linux.IoUring = @ptrCast(self.runner);
-        try uring.submit();
+        const uring: *std.os.linux.IoUring = @ptrCast(@alignCast(self.runner));
+        _ = uring.submit() catch unreachable;
     }
 
     pub fn reap(self: *Async) AsyncError![]Completion {
-        const uring: *std.os.linux.IoUring = @ptrCast(self.runner);
+        const uring: *std.os.linux.IoUring = @ptrCast(@alignCast(self.runner));
         var cqes: [256]std.os.linux.io_uring_cqe = [_]std.os.linux.io_uring_cqe{undefined} ** 256;
-        const count = try uring.copy_cqes(cqes[0..], 1);
+        const count = uring.copy_cqes(cqes[0..], 1) catch unreachable;
 
         const min = @min(self.completions.len, count);
 
@@ -58,9 +56,12 @@ pub const AsyncIoUring = struct {
     pub fn to_async(self: *AsyncIoUring) Async {
         return Async{
             .runner = self.runner,
-            .queue_send = queue_send,
-            .queue_recv = queue_recv,
-            .submit = submit,
+            .completions = self.completions,
+            ._queue_accept = queue_accept,
+            ._queue_send = queue_send,
+            ._queue_recv = queue_recv,
+            ._submit = submit,
+            ._reap = reap,
         };
     }
 };
--- src/async/lib.zig
+++ src/async/lib.zig
@@ -17,11 +17,31 @@ pub const AsyncError = error{};
 
 pub const Async = struct {
     runner: *anyopaque,
-    completions: []Completion,
+    completions: [256]Completion,
 
-    queue_accept: *const fn (self: *Async, context: *anyopaque, socket: std.posix.socket_t) AsyncError!void,
-    queue_send: *const fn (self: *Async, context: *anyopaque, socket: std.posix.socket_t, buffer: []u8) AsyncError!void,
-    queue_recv: *const fn (self: *Async, context: *anyopaque, socket: std.posix.socket_t, buffer: []u8) AsyncError!void,
-    reap: *const fn (self: *Async) AsyncError![]Completion,
-    submit: *const fn (self: *Async) AsyncError!void,
+    _queue_accept: *const fn (self: *Async, context: *anyopaque, socket: std.posix.socket_t) AsyncError!void,
+    _queue_send: *const fn (self: *Async, context: *anyopaque, socket: std.posix.socket_t, buffer: []const u8) AsyncError!void,
+    _queue_recv: *const fn (self: *Async, context: *anyopaque, socket: std.posix.socket_t, buffer: []u8) AsyncError!void,
+    _reap: *const fn (self: *Async) AsyncError![]Completion,
+    _submit: *const fn (self: *Async) AsyncError!void,
+
+    pub fn queue_accept(self: *Async, context: *anyopaque, socket: std.posix.socket_t) AsyncError!void {
+        @call(.auto, self._queue_accept, .{ self, context, socket }) catch unreachable;
+    }
+
+    pub fn queue_send(self: *Async, context: *anyopaque, socket: std.posix.socket_t, buffer: []const u8) AsyncError!void {
+        @call(.auto, self._queue_send, .{ self, context, socket, buffer }) catch unreachable;
+    }
+
+    pub fn queue_recv(self: *Async, context: *anyopaque, socket: std.posix.socket_t, buffer: []u8) AsyncError!void {
+        @call(.auto, self._queue_recv, .{ self, context, socket, buffer }) catch unreachable;
+    }
+
+    pub fn reap(self: *Async) AsyncError![]Completion {
+        return @call(.auto, self._reap, .{self}) catch unreachable;
+    }
+
+    pub fn submit(self: *Async) AsyncError!void {
+        @call(.auto, self._submit, .{self}) catch unreachable;
+    }
 };
