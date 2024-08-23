e016e62ff9f95c5efc8f74ef4bbc4047a141bd24
Muki Kiboigo <muki@kiboigo.com>
2024-08-22 21:52:17 -0700

    fix minram example max uri size

--- src/examples/minram/main.zig
+++ src/examples/minram/main.zig
@@ -37,6 +37,7 @@ pub fn main() !void {
         .size_connections_max = 16,
         .size_context_arena_retain = 64,
         .size_request_max = 2048,
+        .size_request_uri_max = 256,
         .size_socket_buffer = 512,
     }, router);
 
