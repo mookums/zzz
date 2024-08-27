d507ee03c592e93e9d2c89d517ccde9718ba8acb
Muki Kiboigo <muki@kiboigo.com>
2024-08-26 23:19:46 -0700

    tls for big files

--- src/core/job.zig
+++ src/core/job.zig
@@ -11,12 +11,25 @@ const JobType = enum {
     Close,
 };
 
+pub const SendType = union(enum) {
+    Plain: struct {
+        slice: Pseudoslice,
+        count: u32,
+    },
+    TLS: struct {
+        slice: Pseudoslice,
+        count: u32,
+        encrypted: []const u8,
+        encrypted_count: u32,
+    },
+};
+
 pub const Job = union(JobType) {
     Open,
     Read: struct { fd: std.posix.fd_t, count: u32 },
     Write: struct { fd: std.posix.fd_t, count: u32 },
     Accept,
     Recv: struct { count: u32 },
-    Send: struct { slice: Pseudoslice, count: u32 },
+    Send: SendType,
     Close,
 };
