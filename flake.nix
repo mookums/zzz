cbce7e42e3401c37554482d71e7f45b8d89906ba
Muki Kiboigo <muki@kiboigo.com>
2024-09-27 14:57:47 -0700

    update flake

--- flake.nix
+++ flake.nix
@@ -1,41 +1,41 @@
 {
-    description = "zzz is a high performance communicaton abstraction library.";
+  description = "a high performance communicaton abstraction library";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
-        zig.url = "github:mitchellh/zig-overlay";
-        zls.url = "github:zigtools/zls/0.13.0";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig.url = "github:mitchellh/zig-overlay";
+    zls.url = "github:zigtools/zls/0.13.0";
+  };
 
-    outputs = inputs@{self, nixpkgs, ...}: 
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
     let
-        overlays = [
-            (final: prev: rec {
-                zigpkgs = inputs.zig.packages.${prev.system};
-                zig = zigpkgs."0.13.0";
-                zls = inputs.zls.packages.${prev.system}.zls.overrideAttrs (old: {
-                    nativeBuildInputs = [ zig ];
-                });
-            })
-        ];
+      overlays = [
+        (final: prev: rec {
+          zigpkgs = inputs.zig.packages.${prev.system};
+          zig = zigpkgs."0.13.0";
+          zls = inputs.zls.packages.${prev.system}.zls.overrideAttrs
+            (old: { nativeBuildInputs = [ zig ]; });
+        })
+      ];
 
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit overlays system; };
-    in
-    {
-        devShells.${system}.default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-                zig
-                zls
-                # SSL Testing
-                openssl
-                # Debugging
-                gdb
-                valgrind
-                # Benchmarking
-                linuxPackages.perf
-                wrk
-            ];
+      systems = builtins.attrNames inputs.zig.packages;
+    in flake-utils.lib.eachSystem systems (system:
+      let pkgs = import nixpkgs { inherit overlays system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            zig
+            zls
+            # SSL Testing
+            openssl
+            # Debugging
+            gdb
+            valgrind
+            # Benchmarking
+            linuxPackages.perf
+            wrk
+          ];
         };
-    };
+      });
 }
