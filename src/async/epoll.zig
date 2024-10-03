194581c1e78fbd1d41d7aa6a30a18b5bbf6f6004
Muki Kiboigo <muki@kiboigo.com>
2024-10-02 22:27:44 -0700

    fix epoll resource leak

--- src/async/epoll.zig
+++ src/async/epoll.zig
@@ -191,86 +191,82 @@ pub const AsyncEpoll = struct {
             const num_events = std.posix.epoll_wait(epoll.epoll_fd, epoll.events[0..max_events], timeout);
             log.debug("Number of Events: {d}", .{num_events});
 
-            for (epoll.events[0..num_events]) |event| {
-                var release_job = true;
+            epoll_loop: for (epoll.events[0..num_events]) |event| {
                 const job_index = event.data.u64;
+                var release_job = true;
                 defer if (release_job) epoll.jobs.release(job_index);
                 assert(epoll.jobs.dirty.isSet(job_index));
 
-                var result: Completion.Result = undefined;
                 const job = epoll.jobs.items[job_index];
 
-                switch (job.type) {
-                    .accept => {
-                        assert(event.events & std.os.linux.EPOLL.IN != 0);
-                        const accepted_socket = std.posix.accept(job.socket, null, null, 0) catch |e| {
-                            switch (e) {
-                                // This is only allowed here because
-                                // multiple threads are sitting on accept.
-                                // Any other case is unreachable.
-                                error.WouldBlock => {
-                                    release_job = false;
-                                    continue;
-                                },
-                                else => {
-                                    log.debug("accept failed: {}", .{e});
-                                    result = .{ .value = -1 };
-                                    epoll.remove_fd(job.socket) catch unreachable;
-                                    break;
-                                },
-                            }
-                        };
+                const result: Completion.Result = blk: {
+                    switch (job.type) {
+                        .accept => {
+                            assert(event.events & std.os.linux.EPOLL.IN != 0);
+                            const accepted_socket = std.posix.accept(job.socket, null, null, 0) catch |e| {
+                                switch (e) {
+                                    // This is only allowed here because
+                                    // multiple threads are sitting on accept.
+                                    // Any other case is unreachable.
+                                    error.WouldBlock => {
+                                        release_job = false;
+                                        continue :epoll_loop;
+                                    },
+                                    else => {
+                                        log.debug("accept failed: {}", .{e});
+                                        epoll.remove_fd(job.socket) catch unreachable;
+                                        break :blk .{ .socket = -1 };
+                                    },
+                                }
+                            };
 
-                        result = .{ .socket = accepted_socket };
-                        epoll.remove_fd(job.socket) catch unreachable;
-                    },
-                    .recv => |buffer| {
-                        assert(event.events & std.os.linux.EPOLL.IN != 0);
-                        const bytes_read = std.posix.recv(job.socket, buffer, 0) catch |e| {
-                            switch (e) {
-                                error.WouldBlock => unreachable,
-                                error.ConnectionResetByPeer => {
-                                    result = .{ .value = 0 };
-                                    epoll.remove_fd(job.socket) catch unreachable;
-                                    break;
-                                },
-                                else => {
-                                    log.debug("recv failed: {}", .{e});
-                                    result = .{ .value = -1 };
-                                    epoll.remove_fd(job.socket) catch unreachable;
-                                    break;
-                                },
-                            }
-                        };
+                            epoll.remove_fd(job.socket) catch unreachable;
+                            break :blk .{ .socket = accepted_socket };
+                        },
+                        .recv => |buffer| {
+                            assert(event.events & std.os.linux.EPOLL.IN != 0);
+                            const bytes_read = std.posix.recv(job.socket, buffer, 0) catch |e| {
+                                switch (e) {
+                                    error.WouldBlock => unreachable,
+                                    error.ConnectionResetByPeer => {
+                                        epoll.remove_fd(job.socket) catch unreachable;
+                                        break :blk .{ .value = 0 };
+                                    },
+                                    else => {
+                                        log.debug("recv failed: {}", .{e});
+                                        epoll.remove_fd(job.socket) catch unreachable;
+                                        break :blk .{ .value = -1 };
+                                    },
+                                }
+                            };
 
-                        result = .{ .value = @intCast(bytes_read) };
-                    },
-                    .send => |buffer| {
-                        assert(event.events & std.os.linux.EPOLL.OUT != 0);
-                        const bytes_sent = std.posix.send(job.socket, buffer, 0) catch |e| {
-                            switch (e) {
-                                error.WouldBlock => unreachable,
-                                error.ConnectionResetByPeer => {
-                                    result = .{ .value = 0 };
-                                    epoll.remove_fd(job.socket) catch unreachable;
-                                    break;
-                                },
-                                else => {
-                                    log.debug("send failed: {}", .{e});
-                                    result = .{ .value = -1 };
-                                    epoll.remove_fd(job.socket) catch unreachable;
-                                    break;
-                                },
-                            }
-                        };
+                            break :blk .{ .value = @intCast(bytes_read) };
+                        },
+                        .send => |buffer| {
+                            assert(event.events & std.os.linux.EPOLL.OUT != 0);
+                            const bytes_sent = std.posix.send(job.socket, buffer, 0) catch |e| {
+                                switch (e) {
+                                    error.WouldBlock => unreachable,
+                                    error.ConnectionResetByPeer => {
+                                        epoll.remove_fd(job.socket) catch unreachable;
+                                        break :blk .{ .value = 0 };
+                                    },
+                                    else => {
+                                        log.debug("send failed: {}", .{e});
+                                        epoll.remove_fd(job.socket) catch unreachable;
+                                        break :blk .{ .value = -1 };
+                                    },
+                                }
+                            };
 
-                        result = .{ .value = @intCast(bytes_sent) };
-                    },
-                    .close => {
-                        std.posix.close(job.socket);
-                        result = .{ .value = 0 };
-                    },
-                }
+                            break :blk .{ .value = @intCast(bytes_sent) };
+                        },
+                        .close => {
+                            std.posix.close(job.socket);
+                            break :blk .{ .value = 0 };
+                        },
+                    }
+                };
 
                 self.completions[reaped] = .{
                     .result = result,
