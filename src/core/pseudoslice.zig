1fe7ae2ec755a4214f065c8cd73261461a77f555
Muki Kiboigo <muki@kiboigo.com>
2024-10-07 12:36:58 -0700

    fix pseudoslice

--- src/core/pseudoslice.zig
+++ src/core/pseudoslice.zig
@@ -8,69 +8,90 @@ pub const Pseudoslice = struct {
     first: []const u8,
     second: []const u8,
     shared: []u8,
-    len: u32,
+    len: usize,
 
     pub fn init(first: []const u8, second: []const u8, shared: []u8) Pseudoslice {
         return Pseudoslice{
             .first = first,
             .second = second,
             .shared = shared,
-            .len = @intCast(first.len + second.len),
+            .len = first.len + second.len,
         };
     }
 
     /// Operates like a slice. That means it does not capture the end.
     /// Start is an inclusive bound and end is an exclusive bound.
-    pub fn get(self: *Pseudoslice, start: u32, end: u32) []const u8 {
+    pub fn get(self: *Pseudoslice, start: usize, end: usize) []const u8 {
         assert(end >= start);
         assert(self.shared.len >= end - start);
         const clamped_end = @min(end, self.len);
 
         if (start < self.first.len) {
             if (clamped_end <= self.first.len) {
+                // within first slice
                 return self.first[start..clamped_end];
             } else {
-                // Across both buffers
+                // across both slices
                 const first_len = self.first.len - start;
                 const second_len = clamped_end - self.first.len;
-                const total_len = first_len + second_len;
+                const total_len = clamped_end - start;
 
-                // isn't first already in this buffer?
-                @memcpy(self.shared[first_len..total_len], self.second[0..second_len]);
-                return self.shared[0..total_len];
+                if (self.first.ptr == self.shared.ptr) {
+                    // just copy over the second.
+                    std.mem.copyForwards(u8, self.shared[self.first.len..], self.second[0..second_len]);
+                    return self.shared[start..clamped_end];
+                } else {
+                    // copy both over.
+                    std.mem.copyForwards(u8, self.shared[0..first_len], self.first[start..]);
+                    std.mem.copyForwards(u8, self.shared[first_len..], self.second[0..second_len]);
+                    return self.shared[0..total_len];
+                }
             }
-        }
-
-        if (start >= self.first.len) {
+        } else {
+            // within second slice
             const second_start = start - self.first.len;
             const second_end = clamped_end - self.first.len;
             return self.second[second_start..second_end];
         }
-
-        unreachable;
     }
 };
 
 const testing = std.testing;
 
-test "General" {
+test "Pseudoslice General" {
     var buffer = [_]u8{0} ** 1024;
     const value = "hello, my name is muki";
     var pseudo = Pseudoslice.init(value[0..6], value[6..], buffer[0..]);
 
     for (0..pseudo.len) |i| {
         for (0..i) |j| {
-            try testing.expectEqualStrings(value[j..i], pseudo.get(@intCast(j), @intCast(i)));
+            try testing.expectEqualStrings(value[j..i], pseudo.get(j, i));
         }
     }
 }
 
-test "Empty Second" {
+test "Pseudoslice Empty Second" {
     var buffer = [_]u8{0} ** 1024;
     const value = "hello, my name is muki";
     var pseudo = Pseudoslice.init(value[0..], &.{}, buffer[0..]);
 
     for (0..pseudo.len) |i| {
-        try testing.expectEqualStrings(value[0..i], pseudo.get(@intCast(0), @intCast(i)));
+        try testing.expectEqualStrings(value[0..i], pseudo.get(0, i));
+    }
+}
+
+test "Pseudoslice First and Shared Same" {
+    const buffer = try testing.allocator.alloc(u8, 1024);
+    defer testing.allocator.free(buffer);
+
+    const value = "hello, my name is muki";
+    std.mem.copyForwards(u8, buffer, value[0..6]);
+
+    var pseudo = Pseudoslice.init(buffer[0..6], value[6..], buffer);
+
+    for (0..pseudo.len) |i| {
+        for (0..i) |j| {
+            try testing.expectEqualStrings(value[j..i], pseudo.get(j, i));
+        }
     }
 }
