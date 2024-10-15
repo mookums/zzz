6f7770b48693e31043349af69b4186a7a27956fc
Muki Kiboigo <muki@kiboigo.com>
2024-10-15 11:53:58 -0700

    job: change how SendType holds data

--- src/core/job.zig
+++ src/core/job.zig
@@ -1,16 +1,15 @@
 const std = @import("std");
 const Pseudoslice = @import("lib.zig").Pseudoslice;
 
-pub const SendType = union(enum) {
-    plain: struct {
-        slice: Pseudoslice,
-        count: u32,
-    },
-    tls: struct {
-        slice: Pseudoslice,
-        count: u32,
-        encrypted: []const u8,
-        encrypted_count: u32,
+pub const SendType = struct {
+    slice: Pseudoslice,
+    count: usize,
+    security: union(enum) {
+        plain,
+        tls: struct {
+            encrypted: []const u8,
+            encrypted_count: usize,
+        },
     },
 };
 
@@ -20,11 +19,11 @@ pub const Job = union(enum) {
     /// ready to be utilized.
     empty,
     open,
-    read: struct { fd: std.posix.fd_t, count: u32 },
-    write: struct { fd: std.posix.fd_t, count: u32 },
+    read: struct { fd: std.posix.fd_t, count: usize },
+    write: struct { fd: std.posix.fd_t, count: usize },
     accept,
-    handshake: struct { state: enum { recv, send }, count: u32 },
-    recv: struct { count: u32 },
+    handshake: struct { state: enum { recv, send }, count: usize },
+    recv: struct { count: usize },
     send: SendType,
     close,
 };
