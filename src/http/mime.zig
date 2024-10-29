15d9ba7d7ffa6e751a11e70af68703ac39cedcc4
Jafagervik <jorgenafagervik@gmail.com>
2024-10-29 10:10:44 +0100

    added all commmon mime types

--- src/http/mime.zig
+++ src/http/mime.zig
@@ -2,8 +2,6 @@ const std = @import("std");
 const assert = std.debug.assert;
 
 /// HTTP MIME Types.
-/// TODO: Extend this to add in all of the "common" MIME types.
-/// https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
 pub const Mime = struct {
     content_type: []const u8,
     extension: []const u8,
@@ -33,6 +31,12 @@ pub const Mime = struct {
         .description = "AVI: Audio Video Interleave",
     };
 
+    pub const AZW = Mime{
+        .content_type = "applicatgion/vnd.amazon.ebook",
+        .extension = ".azw",
+        .description = "AZW: Amazon Kindle eBook format",
+    };
+
     pub const BIN = Mime{
         .content_type = "application/octet-stream",
         .extension = ".bin",
@@ -43,6 +47,30 @@ pub const Mime = struct {
         .content_type = "image/bmp",
         .extension = ".bmp",
         .description = "Windows OS/2 Bitmap Graphics",
+    };
+
+    pub const BZ = Mime{
+        .content_type = "application/x-bzip",
+        .extension = ".bz",
+        .description = "BZip archive",
+    };
+
+    pub const BZ2 = Mime{
+        .content_type = "application/x-bzip2",
+        .extension = ".bz2",
+        .description = "BZip2 archive",
+    };
+
+    pub const CDA = Mime{
+        .content_type = "application/x-cdf",
+        .extension = ".cda",
+        .description = "CD Audio",
+    };
+
+    pub const CSH = Mime{
+        .content_type = "application/x-csh",
+        .extension = ".csh",
+        .description = "C-Shell script",
     };
 
     pub const CSS = Mime{
@@ -57,6 +85,30 @@ pub const Mime = struct {
         .description = "Comma-seperated values (CSV)",
     };
 
+    pub const DOC = Mime{
+        .content_type = "application/msword",
+        .extension = ".doc",
+        .description = "Microsoft Word",
+    };
+
+    pub const DOCX = Mime{
+        .content_type = "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        .extension = ".docx",
+        .description = "Microsoft Word (OpenXML)",
+    };
+
+    pub const EOT = Mime{
+        .content_type = "application/vnd.ms-fontobject",
+        .extension = ".eot",
+        .description = "MS Embedded OpenType fonts",
+    };
+
+    pub const EPUB = Mime{
+        .content_type = "application/epub+zip",
+        .extension = ".epub",
+        .description = "Electronic Publication",
+    };
+
     pub const GZ = Mime{
         .content_type = "application/gzip",
         .extension = ".gz",
@@ -69,6 +121,12 @@ pub const Mime = struct {
         .description = "Graphics Interchange Format (GIF)",
     };
 
+    pub const HTM = Mime{
+        .content_type = "text/html",
+        .extension = ".htm",
+        .description = "HyperText Markup Language (HTML)",
+    };
+
     pub const HTML = Mime{
         .content_type = "text/html",
         .extension = ".html",
@@ -79,6 +137,18 @@ pub const Mime = struct {
         .content_type = "image/vnd.microsoft.icon",
         .extension = ".ico",
         .description = "Icon Format",
+    };
+
+    pub const ICS = Mime{
+        .content_type = "text/calendar",
+        .extension = ".ics",
+        .description = "iCalendar Format",
+    };
+
+    pub const JAR = Mime{
+        .content_type = "application/java-archive",
+        .extension = ".jar",
+        .description = "Java Archive",
     };
 
     pub const JPEG = Mime{
@@ -99,6 +169,30 @@ pub const Mime = struct {
         .description = "JSON Format",
     };
 
+    pub const JSONLD = Mime{
+        .content_type = "application/ld+json",
+        .extension = ".jsonld",
+        .description = "JSON-LD Format",
+    };
+
+    pub const MID = Mime{
+        .content_type = "audio/midi",
+        .extension = ".mid",
+        .description = "Musical Instrument Digial Interface (MIDI)",
+    };
+
+    pub const MIDI = Mime{
+        .content_type = "audio/x-midi",
+        .extension = ".midi",
+        .description = "Musical Instrument Digial Interface (MIDI)",
+    };
+
+    pub const MJS = Mime{
+        .content_type = "text/javascript",
+        .extension = ".mjs",
+        .description = "Javascript module",
+    };
+
     pub const MP3 = Mime{
         .content_type = "audio/mpeg",
         .extension = ".mp3",
@@ -111,6 +205,66 @@ pub const Mime = struct {
         .description = "MP4 Video",
     };
 
+    pub const MPEG = Mime{
+        .content_type = "video/mpeg",
+        .extension = ".mpeg",
+        .description = "MPEG Video",
+    };
+
+    pub const MPKG = Mime{
+        .content_type = "application/vnd.apple.installer+xml",
+        .extension = ".mpkg",
+        .description = "Apple Installer Package",
+    };
+
+    pub const ODP = Mime{
+        .content_type = "application/vnd.oasis.opendocument.presentation",
+        .extension = ".odp",
+        .description = "OpenDocument Presentation Document",
+    };
+
+    pub const ODS = Mime{
+        .content_type = "application/vnd.oasis.opendocument.spreadsheet",
+        .extension = ".ods",
+        .description = "OpenDocument Spreadsheet Document",
+    };
+
+    pub const ODT = Mime{
+        .content_type = "application/vnd.oasis.opendocument.text",
+        .extension = ".odt",
+        .description = "OpenDocument Text Document",
+    };
+
+    pub const OGA = Mime{
+        .content_type = "audio/ogg",
+        .extension = ".oga",
+        .description = "Ogg audio",
+    };
+
+    pub const OGV = Mime{
+        .content_type = "audio/ogv",
+        .extension = ".ogv",
+        .description = "Ogg video",
+    };
+
+    pub const OGX = Mime{
+        .content_type = "audio/ogx",
+        .extension = ".ogx",
+        .description = "Ogg",
+    };
+
+    pub const OPUS = Mime{
+        .content_type = "audio/ogg",
+        .extension = ".opus",
+        .description = "Opus audio in Ogg container",
+    };
+
+    pub const OTF = Mime{
+        .content_type = "font/otf",
+        .extension = ".otf",
+        .description = "OpenType font",
+    };
+
     pub const PNG = Mime{
         .content_type = "image/png",
         .extension = ".png",
@@ -121,6 +275,30 @@ pub const Mime = struct {
         .content_type = "application/pdf",
         .extension = ".pdf",
         .description = "Adobe Portable Document Format",
+    };
+
+    pub const PHP = Mime{
+        .content_type = "application/x-httpd-php",
+        .extension = ".php",
+        .description = "Hypertext Preprocessor (Personal Home Page)",
+    };
+
+    pub const PPT = Mime{
+        .content_type = "application/vnd.ms-powerpoint",
+        .extension = ".ppt",
+        .description = "Microsoft Powerpoint",
+    };
+
+    pub const PPTX = Mime{
+        .content_type = "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+        .extension = ".pptx",
+        .description = "Microsoft Powerpoint (OpenXML)",
+    };
+
+    pub const RAR = Mime{
+        .content_type = "application/vnd.rar",
+        .extension = ".rar",
+        .description = "RAR archive",
     };
 
     pub const SH = Mime{
@@ -141,6 +319,18 @@ pub const Mime = struct {
         .description = "Tape Archive (TAR)",
     };
 
+    pub const TIFF = Mime{
+        .content_type = "image/tiff",
+        .extension = ".tiff",
+        .description = "Tagged Image File Format (Tiff)",
+    };
+
+    pub const TS = Mime{
+        .content_type = "video/mp2t",
+        .extension = ".ts",
+        .description = "MPEG transport stream",
+    };
+
     pub const TTF = Mime{
         .content_type = "font/ttf",
         .extension = ".ttf",
@@ -153,10 +343,22 @@ pub const Mime = struct {
         .description = "Text (generally ASCII or ISO-8859-n)",
     };
 
+    pub const VSD = Mime{
+        .content_type = "application/vnd.visio",
+        .extension = ".vsd",
+        .description = "Microsoft Visio",
+    };
+
     pub const WAV = Mime{
         .content_type = "audio/wav",
         .extension = ".wav",
         .description = "Waveform Audio Format",
+    };
+
+    pub const WEBA = Mime{
+        .content_type = "audio/webm",
+        .extension = ".weba",
+        .description = "WEBM Audio",
     };
 
     pub const WEBM = Mime{
@@ -177,16 +379,70 @@ pub const Mime = struct {
         .description = "Web Open Font Format (WOFF)",
     };
 
+    pub const WOFF2 = Mime{
+        .content_type = "font/woff2",
+        .extension = ".woff2",
+        .description = "Web Open Font Format (WOFF)",
+    };
+
+    pub const XHTML = Mime{
+        .content_type = "application/xhtml+xml",
+        .extension = ".xhtml",
+        .description = "XHTML",
+    };
+
+    pub const XLS = Mime{
+        .content_type = "application/vnd.ms-excel",
+        .extension = ".xls",
+        .description = "Microsoft Excel",
+    };
+
+    pub const XLSX = Mime{
+        .content_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        .extension = ".xlsx",
+        .description = "Microsoft Excel (OpenXML)",
+    };
+
     pub const XML = Mime{
         .content_type = "application/xml",
         .extension = ".xml",
         .description = "XML",
     };
 
+    pub const XUL = Mime{
+        .content_type = "application/vnd.mozilla.xul+xml",
+        .extension = ".xul",
+        .description = "XUL",
+    };
+
     pub const ZIP = Mime{
         .content_type = "application/zip",
         .extension = ".zip",
         .description = "ZIP Archive",
+    };
+
+    pub const @"3GPA" = Mime{
+        .content_type = "audio/3gpp",
+        .extension = ".3gp",
+        .description = "3GPP audio/vido container",
+    };
+
+    pub const @"3GPV" = Mime{
+        .content_type = "video/3gpp",
+        .extension = ".3gp",
+        .description = "3GPP audio/vido container",
+    };
+
+    pub const @"3G2A" = Mime{
+        .content_type = "audio/3gpp2",
+        .extension = ".3g2",
+        .description = "3GPP2 audio/vido container",
+    };
+
+    pub const @"3G2V" = Mime{
+        .content_type = "video/3gpp2",
+        .extension = ".3g2",
+        .description = "3GPP2 audio/vido container",
     };
 
     pub const @"7Z" = Mime{
@@ -197,14 +453,12 @@ pub const Mime = struct {
 
     fn extension_to_key(extension: []const u8) u64 {
         assert(extension.len > 0);
-        const hash = std.hash.Wyhash.hash(0, extension);
-        return hash;
+        return std.hash.Wyhash.hash(0, extension);
     }
 
     fn content_type_to_key(content_type: []const u8) u64 {
         assert(content_type.len > 0);
-        const hash = std.hash.Wyhash.hash(0, content_type);
-        return hash;
+        return std.hash.Wyhash.hash(0, content_type);
     }
 
     pub fn from_extension(extension: []const u8) Mime {
