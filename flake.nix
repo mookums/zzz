3bb64a831f0f6446de720b16f54ff8817042c2c6
Muki Kiboigo <muki@muki.gg>
2024-12-17 19:47:24 -0800

    chore(nix): update flake

--- flake.nix
+++ flake.nix
@@ -1,40 +1,24 @@
 {
-  description = "a framework for writing performant and reliable networked services";
+  description =
+    "a framework for writing performant and reliable networked services";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    iguana.url = "github:mookums/iguana";
     flake-utils.url = "github:numtide/flake-utils";
-    zig.url = "github:mitchellh/zig-overlay";
-    zls.url = "github:zigtools/zls/0.13.0";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    let
-      overlays = [
-        (final: prev: rec {
-          zigpkgs = inputs.zig.packages.${prev.system};
-          zig = zigpkgs."0.13.0";
-          zls = inputs.zls.packages.${prev.system}.zls.overrideAttrs
-            (old: { nativeBuildInputs = [ zig ]; });
-        })
-      ];
-
-      systems = builtins.attrNames inputs.zig.packages;
-    in flake-utils.lib.eachSystem systems (system:
-      let pkgs = import nixpkgs { inherit overlays system; };
+  outputs = { nixpkgs, iguana, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        iguanaLib = iguana.lib.${system};
       in {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            zig
-            zls
-            # SSL Testing
-            openssl
-            # Debugging
-            gdb
-            valgrind
-            # Benchmarking
-            wrk
-          ];
+        devShells.default = iguanaLib.mkShell {
+          zigVersion = "0.13.0";
+          withZls = true;
+
+          extraPackages = with pkgs; [ openssl wrk ];
         };
       });
 }
